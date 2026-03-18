66789097c5ce5a35cc48fe63920f3b52cf514f09
Mark <you@example.com>
2026-03-18 13:31:43 +0200

    Add core layers for A8

--- Lib.Models.TinyTransformer/Layers/TransformerBlock.cs
+++ Lib.Models.TinyTransformer/Layers/TransformerBlock.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lib.Models.TinyTransformer.Layers
+﻿namespace Lib.Models.TinyTransformer.Layers
 {
-    internal class TransformerBlock
+    public class TransformerBlock
     {
+        private readonly SelfAttentionLayer _attention = new();
+        private readonly FeedForwardLayer _ffn = new();
+
+        public float[][] Forward(float[][] x, dynamic weights, int d)
+        {
+            float[][] attnOut = _attention.Compute(x, weights, d);
+            float[][] result = new float[x.Length][];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                result[i] = _ffn.Compute(attnOut[i], weights, d);
+            }
+
+            return result;
+        }
     }
 }
