b12a0ee90f68fb6d8d144901c63790775bbe7f40
Mark <you@example.com>
2026-03-20 10:17:41 +0200

    feat: implement TinyTransformer core layers, model and configuration

---
+++ Lib.Models.TinyTransformer/Factories/TinyTransformerModelFactory.cs
@@ -0,0 +1,13 @@
+﻿using Lib.Models.TinyTransformer.Configuration;
+using Lib.Models.TinyTransformer.State;
+
+namespace Lib.Models.TinyTransformer.Factories
+{
+    public class TinyTransformerModelFactory
+    {
+        public TinyTransformerModel Create(TinyTransformerConfig config, TinyTransformerWeights weights)
+        {
+            return new TinyTransformerModel(config, weights);
+        }
+    }
+}
